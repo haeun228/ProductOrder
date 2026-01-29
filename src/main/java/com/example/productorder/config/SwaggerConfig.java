cb87466999a1cc02b39b8dfd0c9b6c694346645e
haeun228 <123haeun@naver.com>
2026-01-29 15:23:36 +0900

    chore: Swagger 설정

---
+++ src/main/java/com/example/productorder/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.example.productorder.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Product-Order API")
+                        .version("1.0")
+                        .description("Product-Order API 문서"));
+    }
+}
