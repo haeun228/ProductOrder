c7485e72be35d6816c365dc56b784a40c696e4fa
haeun228 <123haeun@naver.com>
2026-01-29 15:32:58 +0900

    feat: ProductRepository & OrderRepository 추가

---
+++ src/main/java/com/example/productorder/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.example.productorder.repository;
+
+import com.example.productorder.domain.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
---
+++ src/main/java/com/example/productorder/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.example.productorder.repository;
+
+import com.example.productorder.domain.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
